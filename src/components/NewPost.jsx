a8238ae41fffd871ea152a8f1082956092716c5a
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-12 08:52:16 +0900

    상태올리기

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react';
-
 import classes from './NewPost.module.css';
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState('');
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-    //enteredBody = event.target.value; 처럼 직접 할당하는 대신
-    // 위 코드처럼 업데이트 함수를 호출해서 새 상태 값 인자를 넘겨주면 된다.
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
