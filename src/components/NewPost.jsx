c88e235c59b84c84cb9765b618e55fd21f638a8a
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-11 17:31:31 +0900

    useState 작성하기

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState('');
+
   function changeBodyHandler(event) {
-    console.log(event.target.value) // textarea 에 입력한 값 콘솔로 확인
+    setEnteredBody(event.target.value);
+    //enteredBody = event.target.value; 처럼 직접 할당하는 대신
+    // 위 코드처럼 업데이트 함수를 호출해서 새 상태 값 인자를 넘겨주면 된다.
   }
 
   return (
@@ -11,6 +17,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
