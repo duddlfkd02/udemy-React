4abe079510f8ac40dc0c5f9f6b82b38ae5f6fa1e
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-11 09:27:32 +0900

    props 사용해보기

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,10 @@
-const names = ['Maximilian', 'Manuel'];
-
-function Post() {
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
-
+function Post(props) {
 
     return (
         <div>
-            <p>{chosenName}</p>
-            <p>React.js is awesome!</p>
-        </div>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
+        </div >
     );
 }
 
