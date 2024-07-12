b814876beebab591905e573867c30542c5d6bb96
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-12 13:44:32 +0900

    모달창 끄는 방법 3가지

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,8 +6,13 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -17,13 +22,40 @@ function PostsList() {
         setEnteredAuthor(event.target.value);
     }
 
+    //추가 모달 띄우는법 
+    // let modalContent;
+
+    // if (modalContent) {
+    //     modalContent = (
+    //         <Modal onClose={hideModalHandler}>
+    //             <NewPost
+    //                 onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
+    //             />
+    //         </Modal>
+    //     );
+    // }
+
+
+
     return (
         <>
-            <Modal>
+            {/* modalContent */}
+            {/* 모달띄우는 방법 3 */}
+            {/* {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
                 <NewPost
                     onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
                 />
             </Modal>
+            )} */}
+            {modalIsVisible ? (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost
+                        onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
+                    />
+                </Modal>
+            ) : false}
+
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author={enteredAuthor} body={enteredBody} />
