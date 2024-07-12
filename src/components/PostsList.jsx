eb38708a1599fa1ca417df52d304945e36036380
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-12 09:17:37 +0900

    children 속성으로 모달 띄우기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+
 import NewPost from './NewPost';
 import Post from './Post';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList() {
@@ -17,7 +19,11 @@ function PostsList() {
 
     return (
         <>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <Modal>
+                <NewPost
+                    onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
+                />
+            </Modal>
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author={enteredAuthor} body={enteredBody} />
