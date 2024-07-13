92b42435f3d8baa95eb300c035adb080c65e53cd
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-13 15:45:21 +0900

    목록 데이터 표시하기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
@@ -6,19 +6,23 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
+    const [posts, setPosts] = useState([]);
 
+    function addPostHandler(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
     return (
         <>
             {isPosting ? (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onCancel={onStopPosting}
+                        onCancel={onStopPosting} onAddPost={addPostHandler}
                     />
                 </Modal>
             ) : false}
 
             <ul className={classes.posts}>
-                <Post author="Max" body="React.js is awesome!" />
+                {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
             </ul>
         </>
     );
