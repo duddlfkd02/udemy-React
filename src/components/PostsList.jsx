94f5af002f9a2b1eefbe3f6b5ab9ed744734a6b7
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-13 15:17:38 +0900

    submit 버튼 완료

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
@@ -6,31 +6,19 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
 
     return (
         <>
             {isPosting ? (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
                         onCancel={onStopPosting}
                     />
                 </Modal>
             ) : false}
 
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
-                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Max" body="React.js is awesome!" />
             </ul>
         </>
     );
