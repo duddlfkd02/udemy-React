5df3a4c5102717bac00be9403114a85a57c2cd0c
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-08-14 08:52:07 +0900

    fix: PostsList 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Post from "./components/Post";
+import PostsList from "./components/PostsList";
 
 const App = () => {
   return (
     <main>
-      <Post author="작가명1" body="내용입니다1" />
-      <Post author="작가명2" body="내용입니다2" />
-      <Post author="작가명3" body="내용입니다3" />
+      <PostsList />
     </main>
   );
 };
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,36 +1,14 @@
-import { useState } from 'react';
+import Post from "./Post";
+import classes from "./PostsList.module.css";
 
-import NewPost from './NewPost';
-import Post from './Post';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
-
-function PostsList({ isPosting, onStopPosting }) {
-    const [posts, setPosts] = useState([]);
-
-    function addPostHandler(postData) {
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
-    return (
-        <>
-            {isPosting ? (
-                <Modal onClose={onStopPosting}>
-                    <NewPost
-                        onCancel={onStopPosting} onAddPost={addPostHandler}
-                    />
-                </Modal>
-            ) : false}
-            {posts.length > 0 && (
-                <ul className={classes.posts}>
-                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
-                </ul>
-            )}
-            {posts.length === 0 && <div style={{ textAlign: 'center', color: 'white' }}>
-                <h2>There are no posts yet.</h2>
-                <p>Start adding some!</p>
-            </div>}
-        </>
-    );
-}
+const PostsList = () => {
+  return (
+    <ul className={classes.posts}>
+      <Post author="작가명1" body="내용입니다1" />
+      <Post author="작가명2" body="내용입니다2" />
+      <Post author="작가명3" body="내용입니다3" />
+    </ul>
+  );
+};
 
 export default PostsList;
