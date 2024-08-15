56273afa55094ca8a60367ea75017c0c21835efc
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-08-15 09:27:15 +0900

    fix: state 올리기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 const PostsList = () => {
+  const [enterBody, setEnterBody] = useState("");
+  const [enterAuthor, setEnterAuthor] = useState("");
+
+  const bodyChangeHandler = (e) => {
+    setEnterBody(e.target.value);
+  };
+
+  const authorChangeHandler = (e) => {
+    setEnterAuthor(e.target.value);
+  };
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="작가명1" body="내용입니다1" />
+        <Post author={enterAuthor} body={enterBody} />
         <Post author="작가명2" body="내용입니다2" />
         <Post author="작가명3" body="내용입니다3" />
       </ul>
